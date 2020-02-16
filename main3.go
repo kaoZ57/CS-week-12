2afdc06e4b533ab91cd869641b967914199dd0ed
kaoZ57 <57824756+kaoZ57@users.noreply.github.com>
2020-02-16 15:14:05 +0700

    Update main3.go

--- main3.go
+++ main3.go
@@ -1,3 +1,13 @@
 package main
 
 import "fmt"
+
+// fibonacci is a function that returns
+// a function that returns an int.
+func fibonacci() func() int {
+	x, y := 0, 1
+	return func() int {
+		x, y = y, x + y
+		return x
+	}
+}
