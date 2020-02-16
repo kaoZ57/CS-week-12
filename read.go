99d2303ffb53a7f20e38d6fe49bf43f51aa07ed0
kaoZ57 <57824756+kaoZ57@users.noreply.github.com>
2020-02-16 15:02:53 +0700

    Update read.go

--- read.go
+++ read.go
@@ -10,3 +10,21 @@ import (
 func main() {
 	fmt.Println(checkExt(".txt"))
 }
+
+func checkExt(ext string) []string {
+	pathS, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	var files []string
+	filepath.Walk(pathS, func(path string, f os.FileInfo, _ error) error {
+		if !f.IsDir() {
+			r, err := regexp.MatchString(ext, f.Name())
+			if err == nil && r {
+				files = append(files, f.Name())
+			}
+		}
+		return nil
+	})
+	return files
+}
