519628e51cff6cc57edd112c8262a463b24a4a9b
kaoZ57 <57824756+kaoZ57@users.noreply.github.com>
2020-02-16 14:59:54 +0700

    Update readtext_test.go

--- readtext_test.go
+++ readtext_test.go
@@ -1,3 +1,19 @@
 package Create_file
 
 import "testing"
+
+func Test_readtxt(t *testing.T) {
+	tests := []struct {
+		want int
+	}{
+		{3000},
+	}
+	for _, test := range tests {
+
+		got := len(readtxt())
+		if got != test.want {
+			t.Errorf("\nunexpected\n\tgot: %v\n\twant: %v", got, test.want)
+		}
+
+	}
+}
