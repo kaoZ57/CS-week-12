ed3458d562c79002914578e43226e99889305da7
kaoZ57 <57824756+kaoZ57@users.noreply.github.com>
2020-02-16 15:15:44 +0700

    Update Card.go

--- Card.go
+++ Card.go
@@ -5,4 +5,24 @@ import "fmt"
 type card struct {
 	card string 
 } 
+
+func main () {
+	var card int
+	fmt.Print("=")
+	fmt.Scan(&card)
+	if card >= 52 {
+		fmt.Println(card,"Error")
+	}else if card <= 0 {
+		fmt.Println(card,"Error")
+	}else if card <= 13 {
+		fmt.Println(Spades)
+	}else if card <= 26 {
+		fmt.Println(card,"Diamonds")
+	}else if card <= 39 {
+		fmt.Println(card,"Clubs")
+	}else if card <= 52 {
+		fmt.Println("Hearts")
+	}
+}	
+
 	
